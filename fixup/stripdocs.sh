928aecc6ede25b3dbd739c801f60e35251370f03
Rick van Rein <rick@openfortress.nl>
2019-12-18 14:16:02 +0000

    New fixup/stripdocs, mkhere moved from depend->osdepend

---
+++ fixup/stripdocs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Remove documentation files from the rootfs.
+#
+# The default list that is erased may be expanded with time,
+# but is supposed to include all files that are not essential
+# to the running of programs inside a container.  This can be
+# useful to strip down a rootfs to its bare minimum size.
+#
+# From: Rick van Rein <rick@openfortress.nl>
+
+
+rm -rf usr/share/doc usr/share/man usr/share/info
